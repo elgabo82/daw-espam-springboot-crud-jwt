6c7cd69eec3508f8e0fd409b85b808e87b8eeb3e
elgabo82 <gabrielmorejon@gmail.com>
2026-03-21 08:36:35 -0500

    Bases del proyecto

---
+++ src/main/java/com/grupofmo/DawEspamLibrosJwtApplication.java
@@ -0,0 +1,13 @@
+package com.grupofmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawEspamLibrosJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawEspamLibrosJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupofmo/DawEspamLibrosJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupofmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawEspamLibrosJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
