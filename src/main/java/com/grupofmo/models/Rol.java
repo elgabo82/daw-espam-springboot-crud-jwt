7d824e966edf5d8f46ace011eeb98390bba72273
elgabo82 <gabrielmorejon@gmail.com>
2026-03-21 09:25:55 -0500

    Clase Usuario

---
+++ src/main/java/com/grupofmo/models/Rol.java
@@ -0,0 +1,12 @@
+package com.grupofmo.models;
+
+/*
+ * DAW-ESPAM - 2026
+ * Tipo Enumerado: Rol
+ * Gabriel Morejón
+ */
+
+public enum Rol {
+	ADMIN,
+	USER
+}
---
+++ src/main/java/com/grupofmo/models/Usuario.java
@@ -0,0 +1,82 @@
+package com.grupofmo.models;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/*
+ * DAW-ESPAM - 2026
+ * Clase: Usuario (DTO)
+ * Gabriel Morejón
+ */
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "usuarios")
+@Data
+public class Usuario implements UserDetails {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "nombres", nullable = false, length = 120)
+	private String nombresCompletos;
+	
+	@Column(name = "usuario", nullable = false, length = 60, unique = true)
+	private String nombreUsuario;
+	
+	@JsonIgnore
+	@Column(nullable = false)
+	private String clave;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false, length = 20)
+	private Rol rol;
+
+	@Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROL_" + rol.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+	
+
+}
